a51c0de891d28d56f2b56a4e40bf7408c9eb94da
NovaPride <novapride27@gmail.com>
2024-05-01 00:29:53 +0300

    normal-ish refactor aync code

--- src/js/async/async.js
+++ src/js/async/async.js
@@ -1,7 +1,10 @@
 import { dbAdress } from "../constants/constants";
 import { clog } from "../utils/utils";
 
-export function fetchDB(){
+export async function getDB(){
+  //new Promise(setTimeout(() =>{}, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return fetch(dbAdress)
-    .then(data => data.json());
+    .then(data => data.json())
+    .then(db => db);
 }
--- src/js/script.js
+++ src/js/script.js
@@ -6,20 +6,18 @@ import { localImageStartPath } from './constants/constants';
 import header_layout from './modules/header/header_layout';
 import cards_layout from './modules/cards/cards_layout';
 
-import { fetchDB } from './async/async';
+import { getDB } from './async/async';
 
 import { renderCards, addListenerToWrapper } from './modules/cards/cards';
 import addListenersToHeader from './modules/header/header';
 
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   header_layout();
   cards_layout();
-  //header();
-  fetchDB().then(db => {
-    renderCards(db, localImageStartPath);
-    addListenerToWrapper();
-    addListenersToHeader(db);
-  });
-  //cards();
+  
+  const db = await getDB();
+  renderCards(db, localImageStartPath);
+  addListenerToWrapper();
+  addListenersToHeader(db);
 });
